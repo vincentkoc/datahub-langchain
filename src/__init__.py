87f954d4bb9873f7c7441f7a8e28a1f1b91cee10
Vincent Koc <vincentkoc@ieee.org>
2024-11-25 10:45:50 +1100

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,7 @@
 """LLM Observability Package"""
 from .base import LLMModel, LLMRun, LLMChain, LLMObserver, LLMPlatformConnector, LLMMetadataEmitter
 from .platforms.langchain import LangChainObserver
+from .platforms.langsmith import LangSmithConnector
 from .emitters.datahub import DataHubEmitter
 from .emitters.console import ConsoleEmitter
 from .config import ObservabilityConfig, ObservabilitySetup
