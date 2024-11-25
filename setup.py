9c5249b0bb41c9694c702ab594d2e4245d2f81f6
Vincent Koc <vincentkoc@ieee.org>
2024-11-25 17:17:59 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="datahub-langchain",
+    name="llm-lineage",
     version="0.1.0",
-    packages=find_packages(),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     install_requires=[
-        "acryl-datahub[datahub-rest]",
+        "acryl-datahub>=0.12.1.2",
+        "acryl-datahub[datahub-rest]>=0.12.1.2",
         "langchain",
         "langchain-community",
         "langchain-openai",
